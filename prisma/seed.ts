a4146705f89ef89627e91117fa8c5adf97eabbfa
Никита Комаров <nikitakmrv76@gmail.com>
2024-08-25 01:17:18 +0300

    Добавлена БД

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+import { prisma } from './prisma-client';
+
+
+// async function up(){
+//     await prisma.user.createMany({
+//         data:[{
+//             name:
+//         },{}]
+//     })
+// }
+
+// async function down(){
+    
+// }
+
+
+// async function main(){
+//     try{
+//         await up()
+//         await down()
+//     }
+//     catch(e){
+//         console.log(e)
+//     }
+// }
